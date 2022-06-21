1457e62ea2ceae06aebd5ab913eacf91d8d808ae
unknown <you@example.com>
2022-06-21 22:17:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './MyComponents/Home';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import About from './MyComponents/About';
+import Services from './MyComponents/Services';
+import Contact from './MyComponents/Contact';
+// import {Redirect} from 'react-router-dom';
+import NavBar from './MyComponents/NavBar';
+import Discover from './MyComponents/Discover';
+import Discover2 from './MyComponents/Discover2';
+import Search from './MyComponents/Search';
+import { Card2 } from './MyComponents/Card2';
+import Card3 from './MyComponents/Card3';
+import Card4 from './MyComponents/Card4';
+import Card from './MyComponents/Card';
+import LNQN from './MyComponents/LNQN';
+import Lectures from './Lectures';
+import Notifications from './MyComponents/Notifications';
+import Quizzes from './MyComponents/Quizzes';
+import Notes from './MyComponents/Notes';
+import Survey from './MyComponents/Survey';
+import Discover3 from './MyComponents/Discover3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      {/* <Discover /> */}
+      <Discover2 />
+      <Search />
+      {/* <Card /> */}
+      <LNQN />
+      
+      <BrowserRouter>
+        <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/about' element={<About />}/>
+        <Route path='/services' element={<Services />}/>
+        <Route path='/contact' element={<Contact />}/> 
+        <Route path='/trending' element={<Card />}/>
+        <Route path='/lectures' element={<Lectures />}/>
+        <Route path='/notifications' element={<Notifications />}/>
+        <Route path='/quizzes' element={<Quizzes />}/>
+        <Route path='/notes' element={<Notes />}/>
+        <Route path='/survey' element={<Survey />}/>
+        <Route path='/discover' element={<Discover3 />}>
+        
+        
+
+        </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
